42ffa5b54809409a185e5ae4c9dc3a66873cec91
Yulong Tan <yulongtan67@gmail.com>
2019-02-04 18:13:08 -0800

    Updates

---
+++ precache-manifest.7521ce73c027658185770ab83de04e57.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b3aa9317302b1c72508a57ffab76a8fc",
+    "url": "/infocapstone2019/static/media/blood-pact-logo.b3aa9317.png"
+  },
+  {
+    "revision": "4a88fc6ea73b949f37af",
+    "url": "/infocapstone2019/static/js/runtime~main.4a88fc6e.js"
+  },
+  {
+    "revision": "7289ff6b4158d455eb48",
+    "url": "/infocapstone2019/static/js/main.7289ff6b.chunk.js"
+  },
+  {
+    "revision": "e57d543d414f3d3a9608",
+    "url": "/infocapstone2019/static/js/1.e57d543d.chunk.js"
+  },
+  {
+    "revision": "7289ff6b4158d455eb48",
+    "url": "/infocapstone2019/static/css/main.88455514.chunk.css"
+  },
+  {
+    "revision": "e57d543d414f3d3a9608",
+    "url": "/infocapstone2019/static/css/1.a4fef658.chunk.css"
+  },
+  {
+    "revision": "001ba55e660c79bb71649a984b9aa38e",
+    "url": "/infocapstone2019/index.html"
+  }
+];
